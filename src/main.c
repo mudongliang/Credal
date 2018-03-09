528969c07a42760d18e573bebb4debe39b016757
Dongliang Mu <mudongliangabcd@gmail.com>
2018-03-09 13:24:21 -0500

    modify main function to only interpret coredump file

--- src/main.c
+++ src/main.c
@@ -6,28 +6,15 @@
 #include "thread_selection.h"
 
 int main(int argc, char *argv[]){
-    if (argc != 4){
-        fprintf(stderr, "Help: %s coredump binary_path library_path\n", argv[0]);
-        fprintf(stderr, "      Make sure that all the library files are in the directory defined by library_path\n");
+    if (argc != 2){
+        fprintf(stderr, "Help: %s coredump\n", argv[0]);
         exit(0);
     }
     set_core_path(argv[1]);
-    set_bin_path(argv[2]);
-    set_lib_path(argv[3]);
 
     elf_core_info *core_info = parse_core(argv[1]); 
     if(!core_info)
         fprintf(stderr,"The core file is not parsed correctly");
-    
-    elf_binary_info *binary_info = parse_binary(core_info); 
-    if(!binary_info)
-        fprintf(stderr,"The binary file is not parsed correctly");
-    
-    int n = select_thread(core_info, binary_info);
-#ifdef LOG_STATUS
-    fprintf(stdout, "STATE: The crash thread is %d\n", n);
-#endif
 
     destroy_core_info(core_info);
-    destroy_bin_info(binary_info);
 }
